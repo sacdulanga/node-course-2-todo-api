40186231e1717bc0a36c7ce03c4d484679864e07
3LOGIXS\chamith_d <chamith_d@eyepax.com>
2018-05-22 18:03:03 +0530

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b03f02d5be0239e831b7dc0')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b03ee5372088133e822efd5')
+  }, {
+    $set: {
+      name: 'Chamith'
+    },
+    $inc: {
+      age: -2
+    }
+  },{
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+  // client.close();
+});
