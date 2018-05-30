ae373896ab3c271d2db0f91ced239892a0925c2c
3LOGIXS\chamith_d <chamith_d@eyepax.com>
2018-05-30 17:33:00 +0530

    POST web service implimented

--- server/server.js
+++ server/server.js
@@ -2,15 +2,23 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./model/todo');
-var {User} = require('./model/user');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.status(200).send(doc);
+  }, (e) => {
+    res.status(400).send(e)
+  });
 });
 
 app.listen(3000, () => {
